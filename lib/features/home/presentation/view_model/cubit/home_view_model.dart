14acce0008558adceb346b82926bc4f91084db68
abanoub al3bani <apanouprizkalla@gmail.com>
2026-05-11 03:06:51 +0300

    feat: implement getCategries to home feature

--- lib/features/home/presentation/view_model/cubit/home_view_model.dart
+++ lib/features/home/presentation/view_model/cubit/home_view_model.dart
@@ -3,12 +3,20 @@ import 'package:flowers_app/config/base_state/base_state.dart';
 import 'package:flowers_app/features/home/presentation/view_model/states/home_events.dart';
 import 'package:flowers_app/features/home/presentation/view_model/states/home_states.dart';
 import 'package:flowers_app/features/occasions/domain/entities/occasion_entity.dart';
+import 'package:flowers_app/features/occasions/domain/entities/product_entity.dart';
 import 'package:flowers_app/features/occasions/domain/use_cases/occasions_use_case.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeViewModel extends Cubit<HomeStates> {
   final OccasionsUseCase _occasionsUseCase;
-  HomeViewModel(this._occasionsUseCase) : super(HomeStates());
+  final CategoriesUseCase _categeoriesUseCase;
+  final BestSellerUseCase _bestSellerUseCase;
+  HomeViewModel(
+    this._occasionsUseCase,
+    this._categeoriesUseCase,
+    this._bestSellerUseCase,
+  ) : super(HomeStates());
 
   void doEvent(HomeEvents event) {
     switch (event) {
@@ -18,7 +26,7 @@ class HomeViewModel extends Cubit<HomeStates> {
   }
 
   Future<void> _getAllData() async {
-    Future.wait([_getOccasions()]);
+    Future.wait([_getOccasions(), _getBestSeller()]);
   }
 
   Future<void> _getOccasions() async {
@@ -43,6 +51,60 @@ class HomeViewModel extends Cubit<HomeStates> {
         emit(
           state.copyWith(
             occasionsStateParam: BaseState(errorMessage: result.errorMessage),
+          ),
+        );
+    }
+  }
+
+  Future<void> _getBestSeller() async {
+    emit(
+      state.copyWith(
+        bsetSelerStateParam: BaseState<List<ProductEntity>>(isLoading: true),
+      ),
+    );
+    final result = await _bestSellerUseCase();
+    switch (result) {
+      case SuccessBaseResponse<List<ProductEntity>>():
+        emit(
+          state.copyWith(
+            bsetSelerStateParam: BaseState<List<ProductEntity>>(
+              isLoading: false,
+              data: result.data,
+            ),
+          ),
+        );
+        break;
+      case ErrorBaseResponse<List<ProductEntity>>():
+        emit(
+          state.copyWith(
+            bsetSelerStateParam: BaseState(errorMessage: result.errorMessage),
+          ),
+        );
+    }
+  }
+
+  Future<void> _getCategories() async {
+    emit(
+      state.copyWith(
+        categoreyStateParam: BaseState<List<CategoryEntity>>(isLoading: true),
+      ),
+    );
+    final result = await _bestSellerUseCase();
+    switch (result) {
+      case SuccessBaseResponse<List<CategoryEntity>>():
+        emit(
+          state.copyWith(
+            categoreyStateParam: BaseState<List<CategoryEntity>>(
+              isLoading: false,
+              data: result.data,
+            ),
+          ),
+        );
+        break;
+      case ErrorBaseResponse<List<CategoryEntity>>():
+        emit(
+          state.copyWith(
+            categoreyStateParam: BaseState(errorMessage: result.errorMessage),
           ),
         );
     }
--- lib/features/home/presentation/view_model/states/home_states.dart
+++ lib/features/home/presentation/view_model/states/home_states.dart
@@ -1,23 +1,29 @@
 import 'package:equatable/equatable.dart';
 import 'package:flowers_app/config/base_state/base_state.dart';
 import 'package:flowers_app/features/occasions/domain/entities/occasion_entity.dart';
+import 'package:flowers_app/features/occasions/domain/entities/product_entity.dart';
+import 'package:flutter/foundation.dart';
 
 class HomeStates extends Equatable {
   final BaseState<List<OccasionEntity>> occasionsState;
-  final BaseState<BestSellerEntity> bsetSelerState;
+  final BaseState<List<ProductEntity>> bsetSelerState;
+  final BaseState<List<CategoryEntity>> categoreyState;
 
   const HomeStates({
     this.occasionsState = const BaseState(),
     this.bsetSelerState = const BaseState(),
+    this.categoreyState = const BaseState(),
   });
   HomeStates copyWith({
     BaseState<List<OccasionEntity>>? occasionsStateParam,
-    BaseState<BestSellerEntity>? bsetSelerStateParam,
+    BaseState<List<ProductEntity>>? bsetSelerStateParam,
+    BaseState<List<CategoryEntity>>? categoreyStateParam,
   }) => HomeStates(
     occasionsState: occasionsStateParam ?? occasionsState,
     bsetSelerState: bsetSelerStateParam ?? bsetSelerState,
+    categoreyState: categoreyStateParam ?? categoreyState,
   );
 
   @override
-  List<Object?> get props => [occasionsState, bsetSelerState];
+  List<Object?> get props => [occasionsState, bsetSelerState, categoreyState];
 }
