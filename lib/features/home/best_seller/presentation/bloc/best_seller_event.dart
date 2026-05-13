3e0106b5020f6487ef9851e3c638c4f239028eb5
Youssef Singer <youssefsinger2@gmail.com>
2026-05-13 14:06:49 +0300

    feat:done best seller feature without testing

--- lib/features/home/best_seller/presentation/bloc/best_seller_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'best_seller_bloc.dart';
-
-abstract class BestSellerEvent extends Equatable {
-  const BestSellerEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- lib/features/home/best_seller/presentation/bloc/best_seller_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'best_seller_bloc.dart';
-
-abstract class BestSellerState extends Equatable {
-  const BestSellerState();  
-
-  @override
-  List<Object> get props => [];
-}
-class BestSellerInitial extends BestSellerState {}
