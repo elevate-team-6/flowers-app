7e50248bc4aeff3409a08d583dc9ef650f41284e
Abdelmalek <m7244985@gmail.com>
2026-05-15 18:37:26 +0300

    refactor: import app_colors in login_button widget

--- lib/features/auth/login/presentation/widgets/login_button.dart
+++ lib/features/auth/login/presentation/widgets/login_button.dart
@@ -1,6 +1,8 @@
 import 'package:flowers_app/core/utils/app_strings.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/app_colors.dart';
+
 class LoginButton extends StatelessWidget {
   final VoidCallback onPressed;
   final bool isLoading;
