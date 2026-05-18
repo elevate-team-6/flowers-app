c0a22fd0b6f0f1d866b110c4036fd9ee71652dd2
HD <ahmedmohamedemam55@gmail.com>
2026-05-18 22:21:11 +0300

    feat(cart): add CartApiClient and CartRemoteDataSourceImpl for cart operations

---
+++ lib/features/cart/api/api_client/cart_api_client.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flowers_app/core/utils/app_end_points.dart';
+import 'package:flowers_app/features/cart/data/models/request/add_to_cart_request.dart';
+import 'package:flowers_app/features/cart/data/models/request/update_quantity_request.dart';
+import 'package:flowers_app/features/cart/data/models/response/cart_response.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'cart_api_client.g.dart';
+
+@lazySingleton
+@RestApi()
+abstract class CartApiClient {
+  @factoryMethod
+  factory CartApiClient(Dio dio) = _CartApiClient;
+
+  @GET(AppEndPoints.cart)
+  Future<CartResponse> getCart();
+
+  @POST(AppEndPoints.cart)
+  Future<CartResponse> addToCart(@Body() AddToCartRequest request);
+
+  @PUT('${AppEndPoints.cart}/{productId}')
+  Future<CartResponse> updateQuantity(
+    @Path('productId') String productId,
+    @Body() UpdateQuantityRequest request,
+  );
+
+  @DELETE('${AppEndPoints.cart}/{productId}')
+  Future<CartResponse> removeItem(@Path('productId') String productId);
+
+  @DELETE(AppEndPoints.cart)
+  Future<CartResponse> clearCart();
+}
---
+++ lib/features/cart/api/data_sources/cart_remote_data_source_impl.dart
@@ -0,0 +1,47 @@
+import 'package:flowers_app/config/base_response/base_response.dart';
+import 'package:flowers_app/config/error_handler/error_handler.dart';
+import 'package:flowers_app/features/cart/api/api_client/cart_api_client.dart';
+import 'package:flowers_app/features/cart/data/data_sources/cart_remote_data_source_contract.dart';
+import 'package:flowers_app/features/cart/data/models/request/add_to_cart_request.dart';
+import 'package:flowers_app/features/cart/data/models/request/update_quantity_request.dart';
+import 'package:flowers_app/features/cart/data/models/response/cart_response.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: CartRemoteDataSourceContract)
+class CartRemoteDataSourceImpl implements CartRemoteDataSourceContract {
+  final CartApiClient _apiClient;
+  const CartRemoteDataSourceImpl(this._apiClient);
+
+  @override
+  Future<BaseResponse<CartResponse>> getCart() =>
+      ErrorHandler.handleApiCall(() => _apiClient.getCart());
+
+  @override
+  Future<BaseResponse<CartResponse>> addToCart(
+    String productId,
+    int quantity,
+  ) => ErrorHandler.handleApiCall(
+    () => _apiClient.addToCart(
+      AddToCartRequest(product: productId, quantity: quantity),
+    ),
+  );
+
+  @override
+  Future<BaseResponse<CartResponse>> updateQuantity(
+    String productId,
+    int quantity,
+  ) => ErrorHandler.handleApiCall(
+    () => _apiClient.updateQuantity(
+      productId,
+      UpdateQuantityRequest(quantity: quantity),
+    ),
+  );
+
+  @override
+  Future<BaseResponse<CartResponse>> removeItem(String productId) =>
+      ErrorHandler.handleApiCall(() => _apiClient.removeItem(productId));
+
+  @override
+  Future<BaseResponse<CartResponse>> clearCart() =>
+      ErrorHandler.handleApiCall(() => _apiClient.clearCart());
+}
