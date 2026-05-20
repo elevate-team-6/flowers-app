bebefbf273a47a4d86e31ab68436b1c59c5f63ef
HD <ahmedmohamedemam55@gmail.com>
2026-05-20 22:22:35 +0300

    feat(reset-password): implement change password API client and remote data source

---
+++ lib/features/profile/reset_password/api/api_client/change_password_api_client.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:flowers_app/core/utils/app_end_points.dart';
+import 'package:flowers_app/features/profile/reset_password/data/models/request/change_password_request.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'change_password_api_client.g.dart';
+
+@lazySingleton
+@RestApi()
+abstract class ChangePasswordApiClient {
+  @factoryMethod
+  factory ChangePasswordApiClient(Dio dio) = _ChangePasswordApiClient;
+
+  @PATCH(AppEndPoints.changePassword)
+  Future<void> changePassword(@Body() ChangePasswordRequest request);
+}
