30c01d74f63d5b6ee7c731214dabfb8a910a5866
HD <ahmedmohamedemam55@gmail.com>
2026-05-20 22:22:53 +0300

    feat(reset-password): implement change password data source and repository

---
+++ lib/features/profile/reset_password/data/repos/change_password_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:flowers_app/config/base_response/base_response.dart';
+import 'package:flowers_app/features/profile/reset_password/data/data_sources/change_password_remote_data_source_contract.dart';
+import 'package:flowers_app/features/profile/reset_password/domain/repos/change_password_repo_contract.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: ChangePasswordRepoContract)
+class ChangePasswordRepoImpl implements ChangePasswordRepoContract {
+  final ChangePasswordRemoteDataSourceContract _dataSource;
+  const ChangePasswordRepoImpl(this._dataSource);
+
+  @override
+  Future<BaseResponse<void>> changePassword(
+    String password,
+    String newPassword,
+  ) => _dataSource.changePassword(password, newPassword);
+}
