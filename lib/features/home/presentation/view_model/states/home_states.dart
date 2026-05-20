c6cbd64ff4f177a5463d4cccd728b9d60129fa7e
Abdelmalek <m7244985@gmail.com>
2026-05-21 00:17:00 +0300

    feat(home): integrate categories logic into `HomeViewModel` and update state
    - Added `categoryState` to `HomeStates` using `List<CategoryEntity>`.
    - Integrated `GetCategoriesUseCase` into `HomeViewModel` and implemented the `_getCategories` method.
    - Updated `HomeStates` copyWith and props to include the category state.
    - Imported necessary entities and use cases for categories feature.

--- lib/features/home/presentation/view_model/states/home_states.dart
+++ lib/features/home/presentation/view_model/states/home_states.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flowers_app/features/categories/domain/entities/category_entity.dart';
 
 import '../../../../../config/base_state/base_state.dart';
 import '../../../../../core/entities/product_entity.dart';
@@ -7,27 +8,23 @@ import '../../../../occasions/domain/entities/occasion_entity.dart';
 class HomeStates extends Equatable {
   final BaseState<List<OccasionEntity>> occasionsState;
   final BaseState<List<ProductEntity>> bestSellerState;
-  // final BaseState<CategoriesEntity> categoryState;
+  final BaseState<List<CategoryEntity>> categoryState;
 
   const HomeStates({
     this.occasionsState = const BaseState(),
     this.bestSellerState = const BaseState(),
-    // this.categoryState = const BaseState(),
+    this.categoryState = const BaseState(),
   });
   HomeStates copyWith({
     BaseState<List<OccasionEntity>>? occasionsStateParam,
     BaseState<List<ProductEntity>>? bestSellerStateParam,
-    // BaseState<CategoriesEntity>? categoryStateParam,
+    BaseState<List<CategoryEntity>>? categoryStateParam,
   }) => HomeStates(
     occasionsState: occasionsStateParam ?? occasionsState,
     bestSellerState: bestSellerStateParam ?? bestSellerState,
-    // categoryState: categoryStateParam ?? categoryState,
+    categoryState: categoryStateParam ?? categoryState,
   );
 
   @override
-  List<Object?> get props => [
-    occasionsState,
-    bestSellerState,
-    // categoryState
-  ];
+  List<Object?> get props => [occasionsState, bestSellerState, categoryState];
 }
