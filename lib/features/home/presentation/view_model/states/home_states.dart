464585989e9236214518fcfd6af6a38f09ffc6cb
Abdelmalek <m7244985@gmail.com>
2026-05-20 18:38:36 +0300

    feat(home): integrate and enable occasions section in `HomeViewModel` and `HomeScreen`
    - Re-enabled `OccasionsUseCase` and integrated `_getOccasions` logic within `HomeViewModel`.
    - Updated `HomeStates` to include and support `occasionsState`.
    - Un-commented and added `OccasionsHomeSection` to `HomeScreen` using `BlocBuilder`.
    - Temporarily commented out existing tests in `home_view_model_test.dart`.

--- lib/features/home/presentation/view_model/states/home_states.dart
+++ lib/features/home/presentation/view_model/states/home_states.dart
@@ -2,30 +2,31 @@ import 'package:equatable/equatable.dart';
 
 import '../../../../../config/base_state/base_state.dart';
 import '../../../../../core/entities/product_entity.dart';
+import '../../../../occasions/domain/entities/occasion_entity.dart';
 
 class HomeStates extends Equatable {
-  // final BaseState<List<OccasionEntity>> occasionsState;
+  final BaseState<List<OccasionEntity>> occasionsState;
   final BaseState<List<ProductEntity>> bestSellerState;
   // final BaseState<CategoriesEntity> categoryState;
 
   const HomeStates({
-    // this.occasionsState = const BaseState(),
+    this.occasionsState = const BaseState(),
     this.bestSellerState = const BaseState(),
     // this.categoryState = const BaseState(),
   });
   HomeStates copyWith({
-    // BaseState<List<OccasionEntity>>? occasionsStateParam,
+    BaseState<List<OccasionEntity>>? occasionsStateParam,
     BaseState<List<ProductEntity>>? bestSellerStateParam,
     // BaseState<CategoriesEntity>? categoryStateParam,
   }) => HomeStates(
-    // occasionsState: occasionsStateParam ?? occasionsState,
+    occasionsState: occasionsStateParam ?? occasionsState,
     bestSellerState: bestSellerStateParam ?? bestSellerState,
     // categoryState: categoryStateParam ?? categoryState,
   );
 
   @override
   List<Object?> get props => [
-    // occasionsState,
+    occasionsState,
     bestSellerState,
     // categoryState
   ];
