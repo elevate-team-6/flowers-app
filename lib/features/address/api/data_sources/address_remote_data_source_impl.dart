f4158579b7c918a12c78571e558dbad3d9b85e8f
Abdelmalek <m7244985@gmail.com>
2026-06-07 09:46:03 +0300

    feat(address): implement domain and data layer for address management

---
+++ lib/features/address/api/data_sources/address_remote_data_source_impl.dart
@@ -0,0 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flowers_app/config/base_response/base_response.dart';
+import 'package:flowers_app/config/cache/secure_cache_helper.dart';
+import 'package:flowers_app/core/utils/app_constants.dart';
+import 'package:flowers_app/core/utils/app_keys.dart';
+import 'package:flowers_app/core/utils/app_strings.dart';
+import 'package:flowers_app/features/address/api/api_client/address_api_client.dart';
+import 'package:flowers_app/features/address/data/data_sources/address_remote_data_source_contract.dart';
+import 'package:flowers_app/features/address/data/models/address_model.dart';
+import 'package:flowers_app/features/address/data/models/address_response.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: AddressRemoteDataSourceContract)
+class AddressRemoteDataSourceImpl implements AddressRemoteDataSourceContract {
+  final SecureCacheHelper _cacheHelper;
+  final AddressApiClient _apiClient;
+  final FirebaseFirestore _firestore;
+
+  AddressRemoteDataSourceImpl(
+    this._cacheHelper,
+    this._apiClient,
+    this._firestore,
+  );
+
+  DocumentReference<Map<String, dynamic>> _getDefaultAddressDoc(String userId) {
+    return _firestore
+        .collection(AppConstants.usersCollection)
+        .doc(userId)
+        .collection(AppConstants.defaultAddressCollection)
+        .doc(AppConstants.defaultAddressDocId);
+  }
+
+  @override
+  Future<BaseResponse<AddressResponse>> getAddresses() async {
+    try {
+      final response = await _apiClient.getAddresses();
+      return SuccessBaseResponse(response);
+    } catch (e) {
+      return ErrorBaseResponse(e.toString());
+    }
+  }
+
+  @override
+  Future<BaseResponse<AddressResponse>> addAddress(AddressModel address) async {
+    try {
+      final response = await _apiClient.addAddress(address);
+      return SuccessBaseResponse(response);
+    } catch (e) {
+      return ErrorBaseResponse(e.toString());
+    }
+  }
+
+  @override
+  Future<BaseResponse<AddressResponse>> updateAddress(
+    AddressModel address,
+  ) async {
+    try {
+      if (address.id == null) {
+        return ErrorBaseResponse(AppStrings.addressIdRequired.tr());
+      }
+      final response = await _apiClient.updateAddress(address.id!, address);
+      return SuccessBaseResponse(response);
+    } catch (e) {
+      return ErrorBaseResponse(e.toString());
+    }
+  }
+
+  @override
+  Future<BaseResponse<AddressResponse>> deleteAddress(String addressId) async {
+    try {
+      final response = await _apiClient.deleteAddress(addressId);
+
+      // If this was the default address, remove it from Firebase
+      final userId = await _cacheHelper.readData(key: AppKeys.userIdKey);
+      if (userId != null) {
+        final doc = await _getDefaultAddressDoc(userId).get();
+        if (doc.exists &&
+            doc.data()?[AppConstants.firestoreIdField] == addressId) {
+          await _getDefaultAddressDoc(userId).delete();
+        }
+      }
+
+      return SuccessBaseResponse(response);
+    } catch (e) {
+      return ErrorBaseResponse(e.toString());
+    }
+  }
+
+  @override
+  Future<BaseResponse<void>> setDefaultAddress(AddressModel address) async {
+    try {
+      final userId = await _cacheHelper.readData(key: AppKeys.userIdKey);
+      if (userId == null) {
+        return ErrorBaseResponse(AppStrings.userNotFound.tr());
+      }
+
+      await _getDefaultAddressDoc(userId).set(address.toJson());
+      return SuccessBaseResponse(null);
+    } catch (e) {
+      return ErrorBaseResponse(e.toString());
+    }
+  }
+
+  @override
+  Future<BaseResponse<AddressModel?>> getDefaultAddress() async {
+    try {
+      final userId = await _cacheHelper.readData(key: AppKeys.userIdKey);
+      if (userId == null) {
+        return ErrorBaseResponse(AppStrings.userNotFound.tr());
+      }
+
+      final doc = await _getDefaultAddressDoc(userId).get();
+      if (doc.exists && doc.data() != null) {
+        return SuccessBaseResponse(AddressModel.fromJson(doc.data()!));
+      }
+      return SuccessBaseResponse(null);
+    } catch (e) {
+      return ErrorBaseResponse(e.toString());
+    }
+  }
+}
---
+++ lib/features/address/domain/repositories/address_repo_contract.dart
@@ -0,0 +1,17 @@
+import 'package:flowers_app/config/base_response/base_response.dart';
+import 'package:flowers_app/features/address/domain/entities/address_entity.dart';
+import 'package:flowers_app/features/address/domain/entities/city_entity.dart';
+import 'package:flowers_app/features/address/domain/entities/governorate_entity.dart';
+
+abstract class AddressRepoContract {
+  Future<BaseResponse<List<AddressEntity>>> getAddresses();
+  Future<BaseResponse<List<AddressEntity>>> addAddress(AddressEntity address);
+  Future<BaseResponse<List<AddressEntity>>> updateAddress(
+    AddressEntity address,
+  );
+  Future<BaseResponse<List<AddressEntity>>> deleteAddress(String addressId);
+  Future<BaseResponse<void>> setDefaultAddress(AddressEntity address);
+  Future<BaseResponse<AddressEntity?>> getDefaultAddress();
+  Future<BaseResponse<List<GovernorateEntity>>> getGovernorates();
+  Future<BaseResponse<List<CityEntity>>> getCities(String governorateId);
+}
