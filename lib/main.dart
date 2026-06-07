cc9f4e315ca1eaded65e8adbbf153fa3a122165e
Abdelmalek <m7244985@gmail.com>
2026-06-07 11:42:44 +0300

    Refactor: extract Firebase and Auth services and address PR feedback

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
-
 import 'package:bot_toast/bot_toast.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flowers_app/core/utils/app_routes.dart';
 import 'package:flowers_app/core/utils/app_theme.dart';
 import 'package:flutter/material.dart';
@@ -11,39 +7,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'config/cache/hive_helper.dart';
-import 'config/cache/secure_cache_helper.dart';
 import 'config/di/di.dart';
+import 'config/services/auth_service.dart';
+import 'config/services/firebase_service.dart';
 import 'core/utils/app_constants.dart';
-import 'core/utils/app_keys.dart';
 import 'features/auth/forgot-password/presentation/view_model/cubit/forgot_password_view_model.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  // Initialize Firebase
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // Crashlytics - Flutter errors
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-
-  // Crashlytics - Dart async errors
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+  await FirebaseService.init();
 
   configureDependencies();
 
   // Initialize Hive
   await getIt<HiveHelper>().init();
 
-  final secureCacheHelper = getIt<SecureCacheHelper>();
-  final token = await secureCacheHelper.readData(key: AppKeys.tokenKey);
-  final bool isRemembered =
-      await secureCacheHelper.readData(key: AppKeys.rememberMeKey) == 'true';
-  final isLoggedIn = token != null && token.isNotEmpty && isRemembered;
+  final isLoggedIn = await AuthService.isLoggedIn();
+
   runApp(
     EasyLocalization(
       supportedLocales: const [
